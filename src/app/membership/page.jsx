033125c4afc01dae9c9efa071b06acbb4201b47f
ascinate <35849551+ascinate@users.noreply.github.com>
2025-06-06 13:52:54 +0530

    Update page.jsx

--- src/app/membership/page.jsx
+++ src/app/membership/page.jsx
@@ -1,8 +1,7 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 
-
-//image
+// image
 import checkmark from '../assets/check-mark.webp'
 import Image from 'next/image'
 import BannerImage from '../components/BannerImage'
@@ -13,131 +12,132 @@ function page() {
         <>
             <Navbar />
             <section className="about-section float-start w-100">
-
-                <div className="about-banner-section  position-relative float-start w-100">
-                    
+                <div className="about-banner-section position-relative float-start w-100">
                     <h2>Choose Your Plan</h2>
                     <figure>
                         <Image src="/mask-group.jpg" width={1440} height={300} alt="logo" />
                     </figure>
                 </div>
-
             </section>
-            <main className='float-start w-100 pricing-pages main-body'>
-                
-                    <section className=" aboutus-article pricing-containter">
-                        <h2 className='crm-heading'>Pick a plan that&#39;s <br/> right <span> for you</span></h2>
-                        <div className="container table-pricings01">
-                            <div className='row'>
-                                <div className='col-lg-12'>
-                                    <div className='pricing-content'>
 
-                                        <div className='pricing-row-main-badge' id='pricing-row-main-badge'>
-                                            <p>Favorite</p>
-                                        </div>
-                                        <div className='pricing-row'>
-
-                                            <ul className='pricing-list-left'>
-                                                <li className='pricing-list-title-head'><span id='pricing-list-title'>Freemium</span> <span className='badge'>Free</span></li>
-                                                <li className='pricing-list-price'>$0 <small className='pricing-list-price-small'><suv>/mo</suv></small></li>
-                                                <li><button className='pricing-list-btn'>Get Started</button></li>
-                                            </ul>
-                                            <ul className='pricing-list-right'>
-                                                <li className='pricing-list-li'><Image src={checkmark} alt="Checkmark" width={20} height={20} />
-                                                    Local Language Support</li>
-                                                <li className='pricing-list-li'><Image src={checkmark} alt="Checkmark" width={20} height={20} />
-                                                    Built-In SEO for Nigerian Search Trends</li>
-                                                <li className='pricing-list-li'><Image src={checkmark} alt="Checkmark" width={20} height={20} />
-                                                    Editorial Tools for Journalists</li>
-                                                <li className='pricing-list-li'><Image src={checkmark} alt="Checkmark" width={20} height={20} />
-                                                    Social Media Integration</li>
-                                                <li className='pricing-list-li'><Image src={checkmark} alt="Checkmark" width={20} height={20} />
-                                                    Social Media Integration</li>
-                                                <li className='pricing-list-li'><Image src={checkmark} alt="Checkmark" width={20} height={20} />
-                                                    Social Media Integration</li>
-                                            </ul>
-                                        </div>
+            <main className="float-start w-100 pricing-pages main-body">
+                <section className="aboutus-article pricing-containter">
+                    <h2 className="crm-heading">
+                        Pick a plan that&#39;s <br /> right <span> for you</span>
+                    </h2>
+                    <div className="container table-pricings01">
+                        <div className="row justify-content-center">
+                            {/* Freemium Plan */}
+                            <div className="col-lg-4 col-md-6 mb-4">
+                                <div className="pricing-content">
+                                    <div className="pricing-row-main-badge">
+                                        <p>Favorite</p>
+                                    </div>
+                                    <div className="pricing-row">
+                                        <ul className="pricing-list-left">
+                                            <li className="pricing-list-title-head">
+                                                <span>Freemium</span> <span className="badge">Free</span>
+                                            </li>
+                                            <li className="pricing-list-price">
+                                                $0 <small className="pricing-list-price-small"><suv>/mo</suv></small>
+                                            </li>
+                                            <li><button className="pricing-list-btn">Get Started</button></li>
+                                        </ul>
+                                        <ul className="pricing-list-right">
+                                            {[
+                                                "Local Language Support",
+                                                "Built-In SEO for Nigerian Search Trends",
+                                                "Editorial Tools for Journalists",
+                                                "Social Media Integration",
+                                                "Social Media Integration",
+                                                "Social Media Integration"
+                                            ].map((item, i) => (
+                                                <li key={i} className="pricing-list-li">
+                                                    <Image src={checkmark} alt="Checkmark" width={20} height={20} />
+                                                    {item}
+                                                </li>
+                                            ))}
+                                        </ul>
                                     </div>
                                 </div>
                             </div>
 
-
-                            <div className='row'>
-                                <div className='col-lg-12'>
-                                    <div className='pricing-content'>
-
-                                        <div className='pricing-row-main-badge' id='pricing-row-main-badge'>
-                                            <p>Popular</p>
-                                        </div>
-                                        <div className='pricing-row'>
-
-                                            <ul className='pricing-list-left'>
-                                                <li className='pricing-list-title-head'><span id='pricing-list-title'>Premium</span> <span className='badge'>20% off</span></li>
-                                                <li className='pricing-list-price'>$65 <small className='pricing-list-price-small'><suv>/mo</suv></small></li>
-                                                <li><button className='pricing-list-btn'>Get Started</button></li>
-                                            </ul>
-                                            <ul className='pricing-list-right'>
-                                                <li className='pricing-list-li'><Image src={checkmark} alt="Checkmark" width={20} height={20} />
-                                                    Local Language Support</li>
-                                                <li className='pricing-list-li'><Image src={checkmark} alt="Checkmark" width={20} height={20} />
-                                                    Built-In SEO for Nigerian Search Trends</li>
-                                                <li className='pricing-list-li'><Image src={checkmark} alt="Checkmark" width={20} height={20} />
-                                                    Editorial Tools for Journalists</li>
-                                                <li className='pricing-list-li'><Image src={checkmark} alt="Checkmark" width={20} height={20} />
-                                                    Social Media Integration</li>
-                                                <li className='pricing-list-li'><Image src={checkmark} alt="Checkmark" width={20} height={20} />
-                                                    Social Media Integration</li>
-                                                <li className='pricing-list-li'><Image src={checkmark} alt="Checkmark" width={20} height={20} />
-                                                    Social Media Integration</li>
-                                            </ul>
-                                        </div>
+                            {/* Premium Plan */}
+                            <div className="col-lg-4 col-md-6 mb-4">
+                                <div className="pricing-content">
+                                    <div className="pricing-row-main-badge">
+                                        <p>Popular</p>
+                                    </div>
+                                    <div className="pricing-row">
+                                        <ul className="pricing-list-left">
+                                            <li className="pricing-list-title-head">
+                                                <span>Premium</span> <span className="badge">20% off</span>
+                                            </li>
+                                            <li className="pricing-list-price">
+                                                $65 <small className="pricing-list-price-small"><suv>/mo</suv></small>
+                                            </li>
+                                            <li><button className="pricing-list-btn">Get Started</button></li>
+                                        </ul>
+                                        <ul className="pricing-list-right">
+                                            {[
+                                                "Local Language Support",
+                                                "Built-In SEO for Nigerian Search Trends",
+                                                "Editorial Tools for Journalists",
+                                                "Social Media Integration",
+                                                "Social Media Integration",
+                                                "Social Media Integration"
+                                            ].map((item, i) => (
+                                                <li key={i} className="pricing-list-li">
+                                                    <Image src={checkmark} alt="Checkmark" width={20} height={20} />
+                                                    {item}
+                                                </li>
+                                            ))}
+                                        </ul>
                                     </div>
                                 </div>
                             </div>
 
-
-                            <div className='row'>
-                                <div className='col-lg-12'>
-                                    <div className='pricing-content'>
-
-                                        <div className='pricing-row-main-badge' id='pricing-row-main-badge'>
-                                            <p>Popular</p>
-                                        </div>
-                                        <div className='pricing-row'>
-
-                                            <ul className='pricing-list-left'>
-                                                <li className='pricing-list-title-head'><span id='pricing-list-title'>Enterprise</span> <span className='badge'>30% off</span></li>
-                                                <li className='pricing-list-price'>$95 <small className='pricing-list-price-small'><suv>/mo</suv></small></li>
-                                                <li><button className='pricing-list-btn'>Get Started</button></li>
-                                            </ul>
-                                            <ul className='pricing-list-right'>
-                                                <li className='pricing-list-li'><Image src={checkmark} alt="Checkmark" width={20} height={20} />
-                                                    Local Language Support</li>
-                                                <li className='pricing-list-li'><Image src={checkmark} alt="Checkmark" width={20} height={20} />
-                                                    Built-In SEO for Nigerian Search Trends</li>
-                                                <li className='pricing-list-li'><Image src={checkmark} alt="Checkmark" width={20} height={20} />
-                                                    Editorial Tools for Journalists</li>
-                                                <li className='pricing-list-li'><Image src={checkmark} alt="Checkmark" width={20} height={20} />
-                                                    Social Media Integration</li>
-                                                <li className='pricing-list-li'><Image src={checkmark} alt="Checkmark" width={20} height={20} />
-                                                    Social Media Integration</li>
-                                                <li className='pricing-list-li'><Image src={checkmark} alt="Checkmark" width={20} height={20} />
-                                                    Social Media Integration</li>
-                                            </ul>
-                                        </div>
+                            {/* Enterprise Plan */}
+                            <div className="col-lg-4 col-md-6 mb-4">
+                                <div className="pricing-content">
+                                    <div className="pricing-row-main-badge">
+                                        <p>Popular</p>
+                                    </div>
+                                    <div className="pricing-row">
+                                        <ul className="pricing-list-left">
+                                            <li className="pricing-list-title-head">
+                                                <span>Enterprise</span> <span className="badge">30% off</span>
+                                            </li>
+                                            <li className="pricing-list-price">
+                                                $95 <small className="pricing-list-price-small"><suv>/mo</suv></small>
+                                            </li>
+                                            <li><button className="pricing-list-btn">Get Started</button></li>
+                                        </ul>
+                                        <ul className="pricing-list-right">
+                                            {[
+                                                "Local Language Support",
+                                                "Built-In SEO for Nigerian Search Trends",
+                                                "Editorial Tools for Journalists",
+                                                "Social Media Integration",
+                                                "Social Media Integration",
+                                                "Social Media Integration"
+                                            ].map((item, i) => (
+                                                <li key={i} className="pricing-list-li">
+                                                    <Image src={checkmark} alt="Checkmark" width={20} height={20} />
+                                                    {item}
+                                                </li>
+                                            ))}
+                                        </ul>
                                     </div>
                                 </div>
-                            </div>   
-                            
+                            </div>
                         </div>
-                    </section>
-                
+                    </div>
+                </section>
             </main>
-
-
-           <Footer/>
+            <Footer />
         </>
-    )
+    );
 }
 
-export default page
+export default page;
