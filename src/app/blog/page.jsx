9aac3f0e21a25eb0d2044566294bee4e6f0f2178
ski12345491 <abu@ascinatetech.com>
2025-06-23 18:54:58 +0530

    article

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,5 +1,6 @@
-"use client"
+'use client'
 import * as React from 'react';
+import { useState, useEffect } from 'react';
 import { DataGrid } from '@mui/x-data-grid';
 import Navbar from '@/app/components/Navbar';
 import DashLeftMenu from '@/app/components/DashLeftMenu';
@@ -8,100 +9,103 @@ import { MdDeleteOutline } from "react-icons/md";
 import { FaRegEye } from "react-icons/fa";
 import Link from 'next/link';
 
-
 function Page() {
+  const [rows, setRows] = useState([]);
 
+  useEffect(() => {
+    const fetchArticles = async () => {
+      try {
+        const res = await fetch("https://magical-friends-d2761de367.strapiapp.com/api/articles");
+        const json = await res.json();
 
+        const formattedRows = json.data.map((article) => ({
+          id: article.id,
+          name: article.title,
+          date: new Date(article.createdAt).toLocaleDateString(),
+          excerpt: article.description,
+          comment: "0", // You can replace this with real data if available
+        }));
+
+        setRows(formattedRows);
+      } catch (err) {
+        console.error("Failed to fetch articles:", err);
+      }
+    };
+
+    fetchArticles();
+  }, []);
 
   const columns = [
-  {
-    field: 'image',
-    headerName: 'Post Image',
-    width: 100,
-    renderCell: () => (
-      <span className='img-td'>
-        <Image src="/blog5.webp" width={50} height={50} alt="image" />
-      </span>
-    )
-  },
-  { field: 'name', headerName: 'Post Titel', width: 150 },
-  { field: 'date', headerName: 'Date', width: 150 },
-  { field: 'excerpt', headerName: 'Excerpt', width: 150 },
-  { field: 'comment', headerName: 'Comment', width: 100 },
-  {
-    field: 'action',
-    headerName: 'Action',
-    width: 120,
-    renderCell: () => (
-      <div className='d-flex mt-2'>
-        <button type='btton' className='btn btn-del'>
-          <FaRegEye />
-        </button>
-        <button type='btton' className='btn ms-2 btn-danger'>
-          <MdDeleteOutline />
-        </button>
-      </div>
-    )
-  },
-  ];
-
-  
-  const rows = [
-    { id: 1, name: 'Suspendisse tempus felis ', date: "12/12/2025", excerpt: "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical.", comment:"3",  categories: "author" },
-    { id: 2, name: 'Suspendisse tempus felis ', date: "12/12/2025", excerpt: "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical.", comment:"3",  categories: "author" },
-    { id: 3, name: 'Suspendisse tempus felis ', date: "12/12/2025", excerpt: "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical.", comment:"3",  categories: "author" },
-    { id: 4, name: 'Suspendisse tempus felis ', date: "12/12/2025", excerpt: "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical.", comment:"3",  categories: "author" },
-    { id: 5, name: 'Suspendisse tempus felis ', date: "12/12/2025", excerpt: "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical.", comment:"3",  categories: "author" },
-    { id: 6, name: 'Suspendisse tempus felis ', date: "12/12/2025", excerpt: "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical.", comment:"3",  categories: "author" },
-    { id: 7, name: 'Suspendisse tempus felis ', date: "12/12/2025", excerpt: "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical.", comment:"3",  categories: "author" },
-    { id: 8, name: 'Suspendisse tempus felis ', date: "12/12/2025", excerpt: "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical.", comment:"3",  categories: "author" },
-    { id: 9, name: 'Suspendisse tempus felis ', date: "12/12/2025", excerpt: "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical.", comment:"3",  categories: "author" },
+    {
+      field: 'image',
+      headerName: 'Post Image',
+      width: 100,
+      renderCell: () => (
+        <span className='img-td'>
+          <Image src="/blog5.webp" width={50} height={50} alt="image" />
+        </span>
+      )
+    },
+    { field: 'name', headerName: 'Post Title', width: 200 },
+    { field: 'date', headerName: 'Date', width: 150 },
+    { field: 'excerpt', headerName: 'Excerpt', width: 250 },
+    { field: 'comment', headerName: 'Comment', width: 100 },
+    {
+      field: 'action',
+      headerName: 'Action',
+      width: 120,
+      renderCell: () => (
+        <div className='d-flex mt-2'>
+          <button type='button' className='btn btn-del'>
+            <FaRegEye />
+          </button>
+          <button type='button' className='btn ms-2 btn-danger'>
+            <MdDeleteOutline />
+          </button>
+        </div>
+      )
+    },
   ];
 
   return (
     <>
       <Navbar />
-     <main className="float-start w-100 maind-body dashborad-bg120">
+      <main className="float-start w-100 maind-body dashborad-bg120">
         <section className="dash-section crm-pages-dashborad01 float-start w-100 position-relative">
           <div className="container">
             <nav aria-label="breadcrumb">
-                <ol className="breadcrumb mb-2">
-                    <li className="breadcrumb-item"><Link href="/">Home</Link></li>
-                    <li className="breadcrumb-item active">Blog</li>
-                </ol>
+              <ol className="breadcrumb mb-2">
+                <li className="breadcrumb-item"><Link href="/">Home</Link></li>
+                <li className="breadcrumb-item active">Blog</li>
+              </ol>
             </nav>
             <div className="row mt-3 gy-4 gx-lg-5">
               <aside className='col-lg-3'>
                 <DashLeftMenu />
               </aside>
-
               <div className='col-lg-9 '>
-
                 <div className='crm-box1 w-100'>
-                    <div className='d-flex border-bt pt-3 pb-2 px-4 align-items-center w-100 justify-content-between'>
-                        <h4 className='mb-0'> All Post </h4>
-
-                        <button type='button' className='btn btn-adds01'> Add Post </button>
-                    </div>
-                    <div className='mt-3 mx-auto' style={{ height: 400, width: '95%' }}>
-                      <DataGrid
-                        rows={rows}
-                        columns={columns}
-                        pageSize={5}
-                        checkboxSelection
-                        showToolbar
-                      />
-                    </div>
+                  <div className='d-flex border-bt pt-3 pb-2 px-4 align-items-center w-100 justify-content-between'>
+                    <h4 className='mb-0'> All Post </h4>
+                    <button type='button' className='btn btn-adds01'> Add Post </button>
+                  </div>
+                  <div className='mt-3 mx-auto' style={{ height: 400, width: '95%' }}>
+                    <DataGrid
+                      rows={rows}
+                      columns={columns}
+                      pageSize={5}
+                      checkboxSelection
+                      showToolbar
+                    />
+                  </div>
                 </div>
-
               </div>
             </div>
           </div>
         </section>
-      </main >
-
+      </main>
     </>
-  )
+  );
 }
 
 export default Page;
